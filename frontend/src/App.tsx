e07f3078ec32082a01eba7b175c606b68c96f9a3
Wilkozx <wilkoisontheceiling@gmail.com>
2024-06-29 20:14:47 +0100

    made it so buses face the correct way on the road

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,21 +18,21 @@ function App() {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    let bus_icon = L.icon({
-      iconUrl: 'bus.png',
-      iconSize: [50, 50],
-      iconAnchor: [25, 25],
-      popupAnchor: [0, -25],
-    });
-
     const fetchData = async () => {
       try {
         busLayer.clearLayers();
-        const response = await axios.get(`http://localhost:5000/api/data/127`);
+        const response = await axios.get(`http://localhost:5000/api/data/29`);
         buses = (response.data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery']['VehicleActivity']);
     
         for (let i = 0; i < buses.length; i++) {
-              L.marker([buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude'], buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']], {icon: bus_icon}).addTo(busLayer).bindPopup(`Bus ${buses[i]['MonitoredVehicleJourney']['PublishedLineName']}`);
+              let bus_div_icon = L.divIcon({
+                className: 'bus-icon',
+                html: '<img src="bus.png" width="50" height="50" style="transform: rotate('+ (buses[i]['MonitoredVehicleJourney']["Bearing"]-90) +'deg);">',
+                iconSize: [50, 50],
+                iconAnchor: [25, 25],
+                popupAnchor: [0, -25],
+              });
+              L.marker([buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude'], buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']], {icon: bus_div_icon}).addTo(busLayer).bindPopup(`Bus ${buses[i]['MonitoredVehicleJourney']['PublishedLineName']} bearing ${buses[i]['MonitoredVehicleJourney']['Bearing']} degrees.`);
             }
           busLayer.addTo(map);
       }
